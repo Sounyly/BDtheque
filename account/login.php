d5768411f7538ca0b49308b54c0b01cd175d6c31
Sounyly <sounyboy29@gmail.com>
2017-07-22 15:08:13 +0200

    Essaie peu convainquant

--- account/login.php
+++ account/login.php
@@ -1,31 +1,23 @@
 <?php
 require_once '../func/functions.php';
  require_once '../admin/database.php';
-reconnect_from_cookie();
+
  $db = Database::connect();
-if(isset($_SESSION['auth'])){
-    header('Location: account.php');
-    exit();
-}
+
 if(!empty($_POST) && !empty($_POST['username']) && !empty($_POST['password'])){
    
-    $req = $db->prepare('SELECT * FROM membres WHERE (username = :username OR email = :username) AND confirmed_at IS NOT NULL');
+    $req = $db->prepare('SELECT * FROM membres WHERE (username = :username OR email = :username)');
     $req->execute(['username' => $_POST['username']]);
     $user = $req->fetch();
     if(password_verify($_POST['password'], $user->password)){
         $_SESSION['auth'] = $user;
-$_SESSION['flash']['success'] = 'Vous êtes maintenant connecté';
-if($_POST['remember']){
-    $remember_token = str_random(250);
-    $db->prepare('UPDATE membres SET remember_token = ? WHERE id = ?')->execute([$remember_token, $user->id]);
-    Database::disconnect();
-    setcookie('remember', $user->id . '==' . $remember_token . sha1($user->id . 'ratonlaveurs'), time() + 60 * 60 * 24 * 7);
-}
-        header('Location: account.php');
-        exit();
-    }else{
-        $_SESSION['flash']['danger'] = 'Identifiant ou mot de passe incorrecte';
-    }
+    $_SESSION['flash']['success'] = 'Vous êtes maintenant connecté';
+    
+        Database::disconnect();
+    
+            header('Location: account.php');
+            exit();
+        }
 }
 ?>
 <?php require 'header_account.php'; ?>
--- account/register.php
+++ account/register.php
@@ -1,11 +1,11 @@
 <?php
 require_once '../func/functions.php';
 session_start();
-require_once '../admin/database.php';
-$db = Database::connect();
 if(!empty($_POST)){
-    
+    require_once '../admin/database.php';
     $errors = array();
+
+    $db = Database::connect();
     if(empty($_POST['username']) || !preg_match('/^[a-zA-Z0-9_]+$/', $_POST['username'])){
         $errors['username'] = "Votre pseudo n'est pas valide (alphanumérique)";
     } else {//si le pseudo est déja prit
@@ -23,31 +23,29 @@ if(!empty($_POST)){
     if(empty($_POST['email']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
         $errors['email'] = "Votre email n'est pas valide";
     } else {
-       
+       $db = Database::connect();
         $req = $db->prepare('SELECT id FROM membres WHERE email = ?');
         $req->execute([$_POST['email']]);
         $user = $req->fetch();
-        
+          
         if($user){
             $errors['email'] = 'Cet email est déjà utilisé pour un autre compte';
         }
-    }
+    }Database::disconnect();
     if(empty($_POST['password']) || $_POST['password'] != $_POST['password_confirm']){
         $errors['password'] = "Vous devez rentrer un mot de passe valide";
     }
 if(empty($errors)){
-    
-    $req = $db->prepare("INSERT INTO membres SET username = ?, password = ?, email = ?, confirmation_token = ?");
+    $db = Database::connect();
+    $req = $db->prepare("INSERT INTO membres SET username = ?, password = ?, email = ?");
     //cryptage mdp
     $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
     //fonction qui récupere au hasard 60 caracteres de la clef de cryptage, ces caractéres seront
     //ensuite envoyer au visiteur sous forme de lien cliquable qui permet de confirmer/valider l'inscription
-    $token = str_random(60);
-    $req->execute([$_POST['username'], $password, $_POST['email'], $token]);
+    
+    $req->execute([$_POST['username'], $password, $_POST['email']]);
     //lastInsertId renvoie le dernier id générer par pdo
-    $user_id = $db->lastInsertId();
-    mail($_POST['email'], 'Confirmation de votre compte', "Afin de valider votre compte merci de cliquer sur ce lien\n\nhttp://localhost:8888/bdthek/account/confirm.php?id=$user_id&token=$token");
-    $_SESSION['flash']['success'] = 'Un email de confirmation vous a été envoyé pour valider votre compte';
+    
     Database::disconnect();
     header('Location: login.php');
     exit();
